e40cdc2a6608abeef8b283fcd62c805087a9453f
Asbjoern Andersen <r3missus@gmail.com>
2013-10-10 19:42:12 +0200

    second

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodexchange::Application.config.secret_token = '8f4f103dca6840b4f5763a91db1a79f6ad6062eb9048f10bc4e3a77395811c61e65d80688d348eac2e58f2855a0492e8c1a9e101917f85b6856ed17b681395d2'
